a03c4992c4584d84a810153e3e1faafaf859dcaf
mikesams <46354767+mikesams@users.noreply.github.com>
2019-02-05 22:52:55 +0530

    Update setup-master.sh

--- setup-master.sh
+++ setup-master.sh
@@ -85,7 +85,7 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
-kubectl apply -f kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml &>>$LOG 
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/bc79dd1505b0c8681ece4de4c0d86c5cd2643275/Documentation/kube-flannel.yml &>>$LOG 
 Stat $? "Setting Up Flanneld Network"
 sleep 30
 i=120
